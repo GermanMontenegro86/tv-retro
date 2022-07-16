cc6bd3d4a0b4d0055e411ba1be1d1ee1795d80d3
german montenegro <germontenegro715@gmail.com>
2022-07-16 17:11:25 -0300

    inicio

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>Anime y series</title>
 </head>
 
-<!-- contador -->
-
-<center><a href="https://websmultimedia.com/contador-de-visitas-gratis" title="Contador De Visitas">
-<img style="border: 0px solid; display: inline;" alt="contador de visitas" src="https://websmultimedia.com/contador-de-visitas.php?id=3676"></a><br></center>
-
+<!-- Contador de visitas -->
+<center><a href="https://websmultimedia.com/contador-de-visitas-gratis" title="Contador De Visitas Gratis">
+    <img style="border: 0px solid; display: inline;" alt="contador de visitas" src="https://websmultimedia.com/contador-de-visitas.php?id=3681"></a></center>
+    <!-- Fin Contador de visitas -->
 <body class="index">
     <header>
 
@@ -46,7 +46,7 @@
 
     </header>
 
-   
+
 
     <main class="escritorio">
         <section>
@@ -66,34 +66,50 @@
         <div class="series">
             <section>
                 <h2>Anime</h2>
-                <a href="./pages/anime.html"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuqamswQd1hibeowHzd6hvIqhFUeBXfqUsmA&usqp=CAU" alt=""></a>
-                
+                <a href="./pages/anime.html"><img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuqamswQd1hibeowHzd6hvIqhFUeBXfqUsmA&usqp=CAU"
+                        alt=""></a>
+
             </section>
-           
+
             <section>
                 <h2>Series</h2>
-                <a href="./pages/series.html"><img src="https://www.mubis.es/media/users/2509/41314/vuestras-series-favoritas-de-los-80-90-l_cover.jpg" alt=""></a>
-                
+                <a href="./pages/series.html"><img
+                        src="https://www.mubis.es/media/users/2509/41314/vuestras-series-favoritas-de-los-80-90-l_cover.jpg"
+                        alt=""></a>
+
             </section>
 
         </div>
 
-
+        <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
+            <a href=" https://mpago.la/2mNPpdQ"><img class="donacion"
+                src="https://www.diariojuridico.com/wp-content/uploads/2021/08/donaciones-chile-diariojuridico.jpg"
+                alt="fot de una majo depositando una moneda en una caja"></a>
+            
+        </div>
 
 
         <footer class="bg text-center text-lg-start">
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-              © 2022 
-              German Montenegro
+                © 2022
+                German Montenegro
             </div>
             <!-- Copyright -->
-          </footer>
+        </footer>
 
 </body>
+
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
     crossorigin="anonymous"></script>
+
 
 </html>
